586f07386f2f67512364c42bb9ee2f618bcca882
Contex <contexmoh@gmail.com>
2012-10-03 20:29:59 +0200

    Initial commit with base class

---
+++ src/main/java/com/craftfire/bifrostui/BifrostUI.java
@@ -0,0 +1,29 @@
+﻿/*
+ * This file is part of BifrostUI.
+ *
+ * Copyright (c) 2011-2012, CraftFire <http://www.craftfire.com/>
+ * BifrostUI is licensed under the GNU Lesser General Public License.
+ *
+ * BifrostUI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BifrostUI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.craftfire.bifrostui;
+
+public class BifrostUI { 
+
+	public static void main(String[] args)
+	{
+		//TODO: Create an example application using the Bifrost library.
+	}
+
+}
